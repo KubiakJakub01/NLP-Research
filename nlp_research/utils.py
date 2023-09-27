d49f7c2503a30176091cc0f6d1c486c26e216f93
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-27 22:35:58 +0200

    Init utils with logger

---
+++ nlp_research/utils.py
@@ -0,0 +1,41 @@
+"""Utility functions for the project."""
+import logging
+import os
+import random
+
+import coloredlogs
+import torch
+import numpy as np
+
+# Set up logging
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("debug.log", mode="w", encoding="utf-8")
+file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+file_handler.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(
+    coloredlogs.ColoredFormatter("%(asctime)s %(levelname)s %(message)s")
+)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+logger.addHandler(file_handler)
+
+
+def LOG_DEBUG(*args, **kwargs):
+    """Log an debug message."""
+    logger.debug(*args, **kwargs)
+
+
+def LOG_INFO(*args, **kwargs):
+    """Log an info message."""
+    logger.info(*args, **kwargs)
+
+
+def LOG_WARNING(*args, **kwargs):
+    """Log a warning message."""
+    logger.warning(*args, **kwargs)
+
+
+def LOG_ERROR(*args, **kwargs):
+    """Log an error message."""
+    logger.error(*args, **kwargs)
