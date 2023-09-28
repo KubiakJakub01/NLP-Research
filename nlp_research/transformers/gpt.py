29b1d5bb15b2460fa983e0381491a5ac36124452
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-28 22:57:41 +0200

    Add GPT2LMHeadModel model class

--- nlp_research/transformers/gpt.py
+++ nlp_research/transformers/gpt.py
@@ -7,3 +7,82 @@ from transformers.modeling_utils import SequenceSummary
 from transformers.activations import ACT2FN
 from transformers.utils import logging
 from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
+
+from utils import LOG_INFO
+
+
+class GPT2LMHeadModel(GPT2Model):
+    '''GPT2 with language modeling head.'''
+    def __init__(self, config):
+        super().__init__(config)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        self.init_weights()
+
+    def get_output_embeddings(self):
+        '''Get output embeddings.'''
+        return self.lm_head
+
+    def set_output_embeddings(self, new_embeddings):
+        '''Set output embeddings.'''
+        self.lm_head = new_embeddings
+
+    def forward(
+        self,
+        input_ids=None,
+        past_key_values=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        **kwargs
+    ):
+        '''Forward pass of GPT2LMHeadModel.'''
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.transformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            **kwargs,
+        )
+
+        hidden_states = outputs[0]
+
+        lm_logits = self.lm_head(hidden_states)
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+
+            # Flatten the tokens
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        if not return_dict:
+            output = (lm_logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return BaseModelOutputWithPastAndCrossAttentions(
+            loss=loss,
+            logits=lm_logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+            cross_attentions=outputs.cross_attentions,
+        )
