1e8c7f15244a5d8245f467374407b078d2549e46
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-03-05 20:23:54 +0100

    Add CLI argparser

--- slp/whisper_inference.py
+++ slp/whisper_inference.py
@@ -1,7 +1,22 @@
+import argparse
+
 from faster_whisper import WhisperModel
 
+
+def get_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--audio_fp', '-a', type=str, help='Path to audio')
+    parser.add_argument(
+        '--model_size',
+        '-s',
+        type=str,
+        choices=['tiny', 'base', 'small', 'medium', 'large-v1', 'large-v2', 'large-v3'],
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    model_size = 'large-v3'
+    model_size = 'base'
 
     # Run on GPU with FP16
     model = WhisperModel(model_size, device='cuda', compute_type='float16')
