9cb973dde2de4b5e10474cd8042bc653b017ec85
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-08-28 22:19:20 +0200

    Add more args to Seq2Seq

--- nlp_research/trainers/whipser.py
+++ nlp_research/trainers/whipser.py
@@ -12,7 +12,28 @@ def get_whisper_trainer(hparams):
     tokenizer = WhisperTokenizer.from_pretrained(hparams.model_name)
     feature_extractor = WhisperFeatureExtractor.from_pretrained(hparams.model_name)
 
-    training_args = Seq2SeqTrainingArguments(output_dir=hparams.output_dir)
+    training_args = Seq2SeqTrainingArguments(
+        output_dir='./whisper-small-hi',  # change to a repo name of your choice
+        per_device_train_batch_size=16,
+        gradient_accumulation_steps=1,  # increase by 2x for every 2x decrease in batch size
+        learning_rate=1e-5,
+        warmup_steps=500,
+        max_steps=5000,
+        gradient_checkpointing=True,
+        fp16=True,
+        evaluation_strategy='steps',
+        per_device_eval_batch_size=8,
+        predict_with_generate=True,
+        generation_max_length=225,
+        save_steps=1000,
+        eval_steps=1000,
+        logging_steps=25,
+        report_to=['tensorboard'],
+        load_best_model_at_end=True,
+        metric_for_best_model='wer',
+        greater_is_better=False,
+        push_to_hub=True,
+    )
     return Seq2SeqTrainer(
         args=training_args, model=model, tokenizer=tokenizer, feature_extractor=feature_extractor
     )
