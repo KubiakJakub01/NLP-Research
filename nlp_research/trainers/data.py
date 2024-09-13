4c434c69c2aca4b7638e990d31c1081ca4e7f1e7
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-09-13 21:34:46 +0200

    Remove unnecessary columns

--- nlp_research/trainers/data.py
+++ nlp_research/trainers/data.py
@@ -9,3 +9,4 @@ def create_dataloader(hparams):
     dataset['validation'] = load_dataset(
         hparams.dataset_name, split='validation', use_auth_token=hparams.use_auth_token
     )
+    dataset = dataset.remove_columns(hparams.remove_columns)
