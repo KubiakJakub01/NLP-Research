4a672da9cfa7fae00cefb5de05cb744ab3bddf4c
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-09-13 21:36:34 +0200

    Cast audio column to Audio

--- nlp_research/trainers/data.py
+++ nlp_research/trainers/data.py
@@ -1,7 +1,7 @@
-from datasets import DatasetDict, load_dataset
+from datasets import Audio, DatasetDict, load_dataset
 
 
-def create_dataloader(hparams):
+def create_audio_dataset(hparams):
     dataset = DatasetDict()
     dataset['train'] = load_dataset(
         hparams.dataset_name, split='train', use_auth_token=hparams.use_auth_token
@@ -10,3 +10,6 @@ def create_dataloader(hparams):
         hparams.dataset_name, split='validation', use_auth_token=hparams.use_auth_token
     )
     dataset = dataset.remove_columns(hparams.remove_columns)
+    dataset = dataset.cast_column('audio', Audio(hparams.sample_rate))
+
+    return dataset
