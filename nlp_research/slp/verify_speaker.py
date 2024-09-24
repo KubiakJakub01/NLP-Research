cdc07956bf4a840c61ee0f306b06ef6571187fbf
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-09-24 21:50:24 +0200

    Extend cli params

--- nlp_research/slp/verify_speaker.py
+++ nlp_research/slp/verify_speaker.py
@@ -3,6 +3,12 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Verify speaker')
-    parser.add_argument('--input', type=str, help='Input file')
-    parser.add_argument('--output', type=str, help='Output file')
+    parser.add_argument('--input', type=str, help='Input file tsv file')
+    parser.add_argument('--ref_col', type=str, help='Reference column')
+    parser.add_argument('--compare_col', type=str, help='Column to compare')
+    parser.add_argument('--simmilarity_col', type=str, help='Column to store simmilarity')
     return parser.parse_args()
+
+
+def main():
+    pass
