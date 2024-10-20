7a15f1ee6c8018f26fc47e5dbcf11b1e29976131
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-10-20 22:07:51 +0200

    Add VITSHparams typing to VITS model

--- nlp_research/slp/tts/vits.py
+++ nlp_research/slp/tts/vits.py
@@ -1,8 +1,10 @@
 from torch import nn
 
+from .hparams import VITSHparams
+
 
 class VITS(nn.Module):
-    def __init__(self, hparams):
+    def __init__(self, hparams: VITSHparams):
         super().__init__()
         self.hparams = hparams
 
