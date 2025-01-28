5f143ef5a2ae3234019f0979173d6140b8058ebd
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-01-28 21:35:40 +0100

    Add singual value decomposition (SVD) alg

--- nlp_research/deep_ml/linear_algebra.py
+++ nlp_research/deep_ml/linear_algebra.py
@@ -39,3 +39,31 @@ def solve_jacobi(A: np.ndarray, b: np.ndarray, n: int) -> list:
             x_hold[i] = (1 / d_a[i]) * (b[i] - sum(nda[i] * x))
         x = x_hold.copy()
     return x.round(4).tolist()
+
+
+def svd_2x2_singular_values(A: np.ndarray) -> tuple:
+    a = A
+
+    a_t = np.transpose(a)
+    a_2 = a_t @ a
+
+    v = np.eye(2)
+
+    for _ in range(1):
+        if a_2[0, 0] == a_2[1, 1]:
+            theta = np.pi / 4
+        else:
+            theta = 0.5 * np.arctan2(2 * a_2[0, 1], a_2[0, 0] - a_2[1, 1])
+
+        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+
+        d = np.transpose(r) @ a_2 @ r
+        a_2 = d
+        v = v @ r
+
+    s = np.sqrt([d[0, 0], d[1, 1]])
+    s_inv = np.array([[1 / s[0], 0], [0, 1 / s[1]]])
+
+    u = a @ v @ s_inv
+
+    return (u, s, v.T)
