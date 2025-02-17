89a1bac1477e4319a3a5efe26cefc0ff648cb768
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-02-17 21:30:32 +0100

    Add softmax function

--- nlp_research/deep_ml/deep_learning.py
+++ nlp_research/deep_ml/deep_learning.py
@@ -4,3 +4,9 @@ import math
 def sigmoid(z: float) -> float:
     result = 1 / (1 + math.exp(-z))
     return round(result, 4)
+
+
+def softmax(scores: list[float]) -> list[float]:
+    exp_sum = sum(math.exp(s) for s in scores)
+    probabilities = [round(math.exp(s) / exp_sum, 4) for s in scores]
+    return probabilities
