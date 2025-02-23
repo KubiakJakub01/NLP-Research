fd9a6e427a8bcab36c3793a09eb4c4494c50ca47
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-02-23 17:14:44 +0100

    Add get_random_subsets function

--- nlp_research/deep_ml/machine_learning.py
+++ nlp_research/deep_ml/machine_learning.py
@@ -121,3 +121,14 @@ def polynomial_features(X: np.ndarray, degree: int) -> np.ndarray:
         ]
         feats.append(out)
     return np.ndarray(feats)
+
+
+def get_random_subsets(X, y, n_subsets, replacements=True, seed=42):
+    np.random.seed(seed)
+
+    n, _ = X.shape
+    subset_size = n if replacements else n // 2
+    idx = np.array(
+        [np.random.choice(n, subset_size, replace=replacements) for _ in range(n_subsets)]
+    )
+    return [(X[idx][i].tolist(), y[idx][i].tolist()) for i in range(n_subsets)]
