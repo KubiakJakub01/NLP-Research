e70f8cbeddd31180a4d7c3e88a515865e9d3d4a3
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-03-06 20:56:46 +0100

    KL Divergence Between Two Normal Distributions

--- nlp_research/deep_ml/linear_algebra.py
+++ nlp_research/deep_ml/linear_algebra.py
@@ -92,3 +92,9 @@ def rref(matrix):
                 continue
             matrix[j] -= matrix[j, i] * row
     return matrix
+
+
+def kl_divergence_normal(mu_p, sigma_p, mu_q, sigma_q):
+    log_pq = np.log(sigma_q / sigma_p)
+    kl_divergence = log_pq + (sigma_p**2 + (mu_p - mu_q) ** 2) / (2 * sigma_q**2) - 0.5
+    return kl_divergence
