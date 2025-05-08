86b46aa2299fd5727de20385bfdeb74f92116b6f
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-05-08 21:52:25 +0200

    Optimal String Alignment Distance

--- nlp_research/deep_ml/nlp.py
+++ nlp_research/deep_ml/nlp.py
@@ -26,3 +26,50 @@ def compute_tf_idf(corpus, query):
         tfidf.append([round(_tf * _idf, 4) for _tf, _idf in zip(tf, idf, strict=False)])
 
     return tfidf
+
+
+def OSA(source: str, target: str) -> int:
+    """
+    Compute the Optimal String Alignment (OSA) distance between two strings.
+
+    :param source: The source string
+    :param target: The target string
+    :return: The OSA distance between the two strings
+    """
+    len_s = len(source)
+    len_t = len(target)
+
+    d = [[0] * (len_t + 1) for _ in range(len_s + 1)]
+
+    for i in range(len_s + 1):
+        d[i][0] = i
+
+    for j in range(len_t + 1):
+        d[0][j] = j
+
+    for i in range(1, len_s + 1):
+        for j in range(1, len_t + 1):
+            cost = 0 if source[i - 1] == target[j - 1] else 1
+
+            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+            if (
+                i > 1
+                and j > 1
+                and source[i - 1] == target[j - 2]
+                and source[i - 2] == target[j - 1]
+            ):
+                d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)
+
+    for i in range(1, len_s + 1):
+        for j in range(1, len_t + 1):
+            cost = 0 if source[i - 1] == target[j - 1] else 1
+            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+            if (
+                i > 1
+                and j > 1
+                and source[i - 1] == target[j - 2]
+                and source[i - 2] == target[j - 1]
+            ):
+                d[i][j] = min(d[i][j], d[i - 2][j - 2] + 1)
+
+    return d[len_s][len_t]
